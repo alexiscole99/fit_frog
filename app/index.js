5165638f5719bc36226c3b9790856e83ce36d40d
Jayleen Li <jayleenli17@gmail.com>
2021-01-23 22:50:36 -0800

    Add files via upload

--- app/index.js
+++ app/index.js
@@ -4,38 +4,41 @@ import exercise from "exercise";
 
 // Fetch UI elements we will need to change
 let hrLabel = document.getElementById("hrm");
-let updatedLabel = document.getElementById("updated");
+//let updatedLabel = document.getElementById("updated");
+let gayTimeLabel = document.getElementById("gaytime");
 let speedLabel = document.getElementById("speed");
+let enableFlag = false;
 
 // Keep a timestamp of the last reading received. Start when the app is started.
 let lastValueTimestamp = Date.now();
 
 // Initialize the UI with some values
 hrLabel.text = "--";
-updatedLabel.text = "...";
+//updatedLabel.text = "...";
 
 // This function takes a number of milliseconds and returns a string
 // such as "5min ago".
 function convertMsAgoToString(millisecondsAgo) {
-  if (millisecondsAgo < 120*1000) {
-    return Math.round(millisecondsAgo / 1000) + "s ago";
-  }
-  else if (millisecondsAgo < 60*60*1000) {
-    return Math.round(millisecondsAgo / (60*1000)) + "min ago";
-  }
-  else {
-    return Math.round(millisecondsAgo / (60*60*1000)) + "h ago"
-  }
+  let h,m,s;
+  h = Math.floor(millisecondsAgo/1000/60/60);
+  m = Math.floor((millisecondsAgo/1000/60/60 - h)*60);
+  s = Math.floor(((millisecondsAgo/1000/60/60 - h)*60 - m)*60);
+  s < 10 ? s = `0${s}`: s = `${s}`
+  m < 10 ? m = `0${m}`: m = `${m}`
+  h < 10 ? h = `0${h}`: h = `${h}`
+
+  return `${h}:${m}:${s}`;
 }
 
 // This function updates the label on the display that shows when data was last updated.
 function updateDisplay() {
-  if (lastValueTimestamp !== undefined) {
-    updatedLabel.text = convertMsAgoToString(Date.now() - lastValueTimestamp);
-  }
+  // if (lastValueTimestamp !== undefined) {
+  //   updatedLabel.text = convertMsAgoToString(Date.now() - lastValueTimestamp);
+  // }
   
   if (exercise && exercise.stats) {
-    //console.log(JSON.stringify(exercise, null, 2));
+    //change icon
+    document.getElementById("walk-icon").href="gay-walk.png";
     const speed = exercise.stats.speed.current;
     const pace = exercise.stats.pace;
     console.log("speed" + speed);
@@ -48,38 +51,54 @@ function updateDisplay() {
     //threshold at 0.5m/s though because kinda hard to walk inside
     if (speed >= 0.5) {
       console.log("pride")
+      if (lastValueTimestamp == undefined) {
+        lastValueTimestamp = Date.now();
+      }
+      gayTimeLabel.text = convertMsAgoToString(Date.now() - lastValueTimestamp);
+      
       let pride = document.getElementById("pride");
       pride.style.display = "inline";
       let gaymode = document.getElementById("gaymode");
       gaymode.style.display = "inline";
       
       //Animate kinda bad tho LOL 
-      var pridered = document.getElementById("pridered");
-      pridered.animate("enable"); 
-      var prideorange = document.getElementById("prideorange");
-      prideorange.animate("enable"); 
-      var prideyellow = document.getElementById("prideyellow");
-      prideyellow.animate("enable"); 
-      var pridegreen = document.getElementById("pridegreen");
-      pridegreen.animate("enable"); 
-      var prideblue = document.getElementById("prideblue");
-      prideblue.animate("enable"); 
-      var pridepurple = document.getElementById("pridepurple");
-      pridepurple.animate("enable"); 
-      var gayModeLabel = document.getElementById("gayModeLabel");
-      gayModeLabel.animate("enable");
-      var frog = document.getElementById("frog");
-      frog.animate("enable");
-      
-      // hide background writing
-      hrLabel.style.display = "none";
-      updatedLabel.style.display = "none";
-      speedLabel.style.display = "none";
+      if (enableFlag === false) {
+        enableFlag = true;
+        var pridered = document.getElementById("pridered");
+        pridered.animate("enable"); 
+        var prideorange = document.getElementById("prideorange");
+        prideorange.animate("enable"); 
+        var prideyellow = document.getElementById("prideyellow");
+        prideyellow.animate("enable"); 
+        var pridegreen = document.getElementById("pridegreen");
+        pridegreen.animate("enable"); 
+        var prideblue = document.getElementById("prideblue");
+        prideblue.animate("enable"); 
+        var pridepurple = document.getElementById("pridepurple");
+        pridepurple.animate("enable"); 
+        var gayModeLabel = document.getElementById("gayModeLabel");
+        gayModeLabel.animate("enable");
+        var frog = document.getElementById("frog");
+        frog.animate("enable");
+
+        // hide background writing
+        hrLabel.style.display = "none";
+        updatedLabel.style.display = "none";
+        speedLabel.style.display = "none";
+      }
+      else {
+        enableFlag = false;
+      }
       
     }
     else {
+      if (lastValueTimestamp !== undefined) {
+        gayTimeLabel.text = "Last gay walk lasted: " + convertMsAgoToString(Date.now() - lastValueTimestamp) + "!";
+        lastValueTimestamp = undefined
+      }
       let pride = document.getElementById("pride");
       pride.style.display = "none";
+      document.getElementById("walk-icon").href="walk-icon.png";
       var gaymode = document.getElementById("gaymode");
       gaymode.style.display = "none";
       hrLabel.style.display = "inline";
@@ -99,8 +118,8 @@ var hrm = new HeartRateSensor();
 hrm.onreading = function() {
   // Peek the current sensor values
   console.log("Current heart rate: " + hrm.heartRate);
-  hrLabel.text = hrm.heartRate;
-  lastValueTimestamp = Date.now();
+  hrLabel.text = "♥ " + hrm.heartRate;
+  lastValueTimestamp = undefined
 }
 
 // Begin monitoring the sensor
